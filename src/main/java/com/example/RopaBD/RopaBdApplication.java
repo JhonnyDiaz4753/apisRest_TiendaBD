d0e0631abf44f2e877af6e4c62c34cfd798c4536
JhonnyDiaz4753 <jhonnydiaz@estudiante.uniajc.edu.co>
2025-10-18 21:01:53 -0500

    primer commit

---
+++ src/main/java/com/example/RopaBD/RopaBdApplication.java
@@ -0,0 +1,13 @@
+package com.example.RopaBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RopaBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RopaBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RopaBD/RopaBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RopaBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RopaBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
